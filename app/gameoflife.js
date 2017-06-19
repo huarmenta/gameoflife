c8c772464fb6407b05749ee465c9a771fc252978
Alex Armenta <h.alexarmenta@gmail.com>
2017-06-18 22:21:21 -0500

    Refactor getLivingNeighbors method
    Uses ternary operators instead of if statements

--- app/gameoflife.js
+++ app/gameoflife.js
@@ -2,13 +2,12 @@
  * @Author: alex
  * @Date:   2017-06-17 16:24:13
  * @Last Modified by:   Alex Armenta
- * @Last Modified time: 2017-06-18 18:55:25
+ * @Last Modified time: 2017-06-18 20:50:52
  */
 
 class Board {
     /**
      * ["Game of life" board class]
-     * @return {} [dictionary with all the cells added to the board]
      */
     constructor() {
         // Sets the board structure with a dictionary.
@@ -16,35 +15,26 @@ class Board {
     }
     addCell(cell) {
         // Adds a new cell to the board.
-        this.cells[this.cellFormat(cell.x, cell.y)] = cell;
+        this.cells[this.formattedCell(cell.x, cell.y)] = cell;
     }
     getCell(x, y) {
         // Returns a cell at the given coordinates x and y.
-        return this.cells[this.cellFormat(x, y)];
+        return this.cells[this.formattedCell(x, y)];
     }
-    cellFormat(x, y) {
-        // Defines the cell format e.g. x1y1
+    formattedCell(x, y) {
+        // Defines the cell format e.g. x0x0, x0y1, x1y1
         return "x" + x + "y" + y;
     }
     getLivingNeighbors(cell) {
         // Returns all the living cells around a given cell.
-        let x = cell.x;
-        let y = cell.y;
         let livingCells = 0;
-        let currentCell;
-
-        // Find a way to refactor this without for loops (maybe recursion)
+        // Find a way to refactor this without for loops (recursion?)
         for (var i = -1; i <= 1; i++) {
             for (var j = -1; j <= 1; j++) {
-                if (i === 0 && i == j) {
-                    continue;
-                }
-                currentCell = this.getCell(x + i, y + j);
+                let currentCell = (i === 0 && j === 0) ? false : this.getCell(cell.x + i, cell.y + j);
                 currentCell && currentCell.isAlive() ? livingCells++ : 0;
             }
         }
-        // /////////////////////////////////////////////////////////////////
-
         return livingCells;
     }
     nextGeneration() {
@@ -62,7 +52,6 @@ class Board {
         }
         this.cells = newBoard;
     }
-
 }
 
 /**
