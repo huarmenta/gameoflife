c13897a2e3ef36e3b58f81e4960329bc398addcc
Hugo Armenta <hugo.armenta@agilekoding.com>
2017-06-21 14:12:57 -0500

    Create cell component

---
+++ src/components/Cell.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Alex Armenta
+ * @Date:   2017-06-20 20:48:35
+ * @Last Modified by:   Alex Armenta
+ * @Last Modified time: 2017-06-20 21:55:26
+ */
+import React from 'react';
+
+class CellComponent extends React.Component {
+    render() {
+        return <div className="cell"></div>;
+    }
+}
+
+export default CellComponent
