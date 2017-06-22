ccf32f709ccb965c3c769b8f044e38cacd43d262
Alex Armenta <h.alexarmenta@gmail.com>
2017-06-21 23:07:29 -0500

    Add Cell object props functionality

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,13 +2,29 @@
  * @Author: Alex Armenta
  * @Date:   2017-06-20 20:48:35
  * @Last Modified by:   Alex Armenta
- * @Last Modified time: 2017-06-20 21:55:26
+ * @Last Modified time: 2017-06-21 22:58:28
  */
 import React from 'react';
+import classNames from 'classnames'
 
 class CellComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.cell = props.cell;
+        this.state = {isAlive: this.cell.alive };
+        this.invertAlive = this.invertAlive.bind(this);
+    }
+    invertAlive() {
+        this.cell.alive = !this.cell.alive;
+        this.setState({isAlive: this.cell.alive});
+    }
     render() {
-        return <div id={this.props.id} className={this.props.className}></div>;
+        let classnames = classNames("cell", this.state.isAlive ? "alive" : "");
+        return (
+                <div id={this.props.id}
+                    className={classnames}
+                    onClick={this.invertAlive}/>
+            );
     }
 }
 
