b1a20b6f63b32dd9feac5002f70a625b90101321
Alex Armenta <h.alexarmenta@gmail.com>
2017-06-23 00:40:51 -0500

    Fix cell component structure to manage in the parent component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,24 +2,27 @@
  * @Author: Alex Armenta
  * @Date:   2017-06-20 20:48:35
  * @Last Modified by:   Alex Armenta
- * @Last Modified time: 2017-06-21 23:51:32
+ * @Last Modified time: 2017-06-23 00:22:35
  */
 import React from 'react';
 import classNames from 'classnames'
 
 class CellComponent extends React.Component {
+    /**
+     * [Creates the Cell component]
+     * @param  {[cell]} cell  [Bind the cell object to the component]
+     */
     constructor(props){
         super(props);
         this.cell = props.cell;
-        this.state = {isAlive: this.cell.isAlive()};
         this.invertAlive = this.invertAlive.bind(this);
     }
     invertAlive() {
-        this.cell.alive = !this.cell.alive;
-        this.setState({isAlive: this.cell.isAlive()});
+        // Sets the invert state of the cell alive attribute.
+        this.props.onCellClick(this.cell);
     }
     render() {
-        let classnames = classNames("cell", this.state.isAlive ? "alive" : "");
+        let classnames = classNames("cell", this.props.cell.isAlive() ? "alive" : "");
         return (
             <div id={this.props.id}
                 className={classnames}
