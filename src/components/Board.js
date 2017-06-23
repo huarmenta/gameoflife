cc27fc197b237ac8f29dbace4ba1b2ed414671ab
Alex Armenta <h.alexarmenta@gmail.com>
2017-06-23 00:40:06 -0500

    Bind board methods including Play,Stop,Step,Clear

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,48 +2,113 @@
  * @Author: Alex Armenta
  * @Date:   2017-06-20 20:48:35
  * @Last Modified by:   Alex Armenta
- * @Last Modified time: 2017-06-21 23:49:29
+ * @Last Modified time: 2017-06-23 00:21:26
  */
 import React from 'react';
 import {Board} from "../app/gameoflife.js"
 import CellComponent from "./Cell"
+import classNames from 'classnames'
 
 class BoardComponent extends React.Component {
+    /**
+     * [Class to create Board component,
+     * this component has rows and cells rendered too,
+     * and buttons that manage its cell states]
+     * @param  {[board]} board   [Board object]
+     */
     constructor(props) {
         super(props);
         this.board = new Board(50, 50);
-        this.board.createBoard();
-        this.state = {cells: this.board.cells}
+        this.board.createBoard(); // Creates cells on board
+        this.state = {
+            board: this.board,
+            running: false,
+            generation: 0
+        }
+        this.tick = this.tick.bind(this);
+        this.setCellAlive = this.setCellAlive.bind(this);
+        this.playBoard = this.playBoard.bind(this);
+        this.stopBoard = this.stopBoard.bind(this);
+        this.nextStep = this.nextStep.bind(this);
+        this.clearBoard = this.clearBoard.bind(this);
     }
-    /*componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 1000);
+    setCellAlive(cell) {
+        // This method sets the new alive attribute to a cell and changes its state.
+        this.state.board.cells[cell.getFormattedCell()].alive = !this.state.board.cells[cell.getFormattedCell()].alive;
+        this.setState({board: this.state.board});
     }
-    componentWillUnmount() {
+    playBoard() {
+        // Starts the process of get new generations of the board cells.
+        this.timerID = setInterval(this.tick, 1);
+        this.setState({running: true});
+    }
+    stopBoard() {
+        // Stops the process of generations.
         clearInterval(this.timerID);
+        this.setState({running: false});
+    }
+    nextStep() {
+        // Sets the next generation step of the board cells.
+        this.tick();
+    }
+    clearBoard() {
+        // Sets all the alive cells to dead cells.
+        this.state.board.clearBoard();
+        this.setState(
+            {
+                board: this.state.board,
+                generation: 0
+            }
+        );
     }
     tick() {
-        this.board.nextGeneration();
-        this.setState({cells: this.board.cells});
-    }*/
+        // Calls the generation process steps of the board.
+        this.state.board.nextGeneration();
+        this.setState(
+            {
+                board: this.state.board,
+                generation: this.state.generation + 1
+            }
+        );
+    }
     rowCells() {
-      let rows = [...Array(this.board.rows).keys()];
-      let cols = [...Array(this.board.rows).keys()];
-      return rows.map((row) => {
-       return <div key={row} className="row">
-          {
-            cols.map((col) => {
-              let cell = this.board.getCell(row, col);
-              return <CellComponent id={cell.getFormatteCell()} key={cell.getFormatteCell()} cell={cell}/>
-            })
-          }
-          </div>
+        // Renders all the cells in its correspondant row.
+        let rows = [...Array(this.state.board.rows).keys()];
+        let cols = [...Array(this.state.board.cols).keys()];
 
-      })
+        return rows.map(row => {
+            return (
+                <div key={row} className="row">
+                {
+                    cols.map(col => {
+                        let cell = this.state.board.getCell(row, col);
+                        return  (
+                            <CellComponent id={cell.getFormattedCell}
+                                key={cell.getFormattedCell()}
+                                cell={cell}
+                                onCellClick={this.setCellAlive}/>
+                        )
+                    })
+                }
+                </div>
+            );
+        });
     }
     render() {
+        let buttonClasses = classNames("button", this.state.running ? "stop" : "play");
         return (
-            <div id="board" className={this.props.className}>
-                {this.rowCells()}
+            <div id="top_left_div">
+                <div id="board" className={this.props.className}>
+                    {this.rowCells()}
+                </div>
+                <div id="buttons_div">
+                    <h3>Generation: {this.state.generation}</h3>
+                    <button onClick={this.state.running ? this.stopBoard : this.playBoard} className={buttonClasses}>
+                        {this.state.running ? "Stop" : "Play"}
+                    </button>
+                    <button onClick={!this.state.running && this.nextStep} className="button step">Step</button>
+                    <button onClick={!this.state.running && this.clearBoard} className="button clear">Clear</button>
+                </div>
             </div>
         );
     }
